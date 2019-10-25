38e9d08ddefd0168f6737c3a1ef4a63b8cd0ee06
Pratik Patil <pppatil@yorku.ca>
2019-10-25 18:06:08 -0400

    auction contract added

--- auction.sol
+++ auction.sol
@@ -1,18 +1,27 @@
 pragma solidity ^0.5.11;
 contract Auction{
-    enum 
+    function Auction()public{
+      max_bidders=4;
+      max_items=3;
+      biddersCount=0;
+      bidders=new Person [](max_bidders);
+      items=new Item[](max_items);
+    }
     struct Item{
         uint itemId;
         uint[] itemTokens;
     }
     struct Person{
+        uint    tokenId;
         address pAddress;
         uint[]  tokenBal;
     }
+    uint public max_bidders;
+    uint public max_items;
+    uint public biddersCount;
     Person[] public bidders;
-    Item[] memory public items;
-    constructor() public {
-        items=new Item[](3);
-        bidders=new Person[](4);
-    }
+    Item[] public items;
+    
+    
+    
 }
